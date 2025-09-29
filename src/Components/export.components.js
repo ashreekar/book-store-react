813951308b679bb39102ff7ccdcac858c6d99df4
ashreekar <ashreekar767@gmail.com>
2025-09-29 20:33:56 +0530

    add components export

---
+++ src/Components/export.components.js
@@ -0,0 +1,8 @@
+import LandingPage from "./LandingPage";
+import BrowseBooks from "./BrowseBooks";
+import BookDetails from "./BookDetails";
+import AddBook from "./AddBook";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export { LandingPage, BrowseBooks, BookDetails, AddBook, Header, Footer };
