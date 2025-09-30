20c83fd8daa15c8ad6358db2a712c26dd24360cc
ashreekar <ashreekar767@gmail.com>
2025-09-30 08:22:47 +0530

    add browse book and style with changes

--- src/Components/BrowseBooks.jsx
+++ src/Components/BrowseBooks.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams,Link } from 'react-router-dom'
+import { booksData } from '../utils/bookData.js';
 
 function BrowseBooks() {
-  return (
-    <div>BrowseBooks</div>
-  )
+  const categoryParam = useParams();
+
+  const [categories, setCategories] = useState([]);
+  const [renderBooks, setRenderBooks] = useState([]);
+
+  useEffect(() => {
+    const uniqueCategory = new Set();
+
+    booksData.forEach((book) => {
+      uniqueCategory.add(book.categories);
+    })
+
+    setCategories([...uniqueCategory]);
+  }, [])
+
+  useEffect(() => {
+    let newBooks = [];
+
+    if (categoryParam.category === 'all') {
+      newBooks = booksData;
+    } else {
+      newBooks = booksData.filter((book, _) => {
+        // Filtering only selected category books
+        return book.categories.toLowerCase() === categoryParam.category.toLowerCase();
+      });
+    }
+
+    setRenderBooks(newBooks);
+  }, [categoryParam])
+
+
+return (
+  <div>Books browse</div>
+)
 }
 
 export default BrowseBooks
